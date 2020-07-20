88ef4b01ee0b49fe7a2e85811ab130dda92c24a0
fogo5000 <lucaslucas2008@gmail.com>
2020-07-20 15:33:48 -0300

    Do feedback screen without gravatar fix Redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-//import './App.css';
+import './App.css';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
