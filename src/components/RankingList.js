b1149f7f28f991bd9faa6b1d6b67b9fdce041585
gabriellukke <gabriellukke@outlook.com>
2020-07-27 13:57:55 -0300

    Ranking Page done

--- src/components/RankingList.js
+++ src/components/RankingList.js
@@ -1,38 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { getLS, setLS } from '../helpers';
 
 class RankingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.insertRanking = this.insertRanking.bind(this);
+  }
+
+  insertRanking() {
+    const { player: { name, score, gravatarEmail } } = getLS('state');
+    //const lsPlayer = getLS('player');
+
+    const rankingOnLS = getLS('ranking');
+    let newRanking = [];
+
+    if (rankingOnLS) {
+      newRanking = [...rankingOnLS, { name, score, gravatarEmail }]
+    } else {
+      newRanking = [{name, score, gravatarEmail}];
+    }
+
+    newRanking.sort((a, b) => b.score - a.score);
+
+    setLS('ranking', newRanking);
+    return newRanking;
+  }
 
   render() {
-    const { avatar, name, score, index } = this.props;
+    const ranking = this.insertRanking();
+    const defaultImg = 'https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3';
     return (
       <div>
         <ul>
-          <li>
-            <img src={avatar} alt={`${name} Avatar`} />
-            <p data-testid={`player-name-${index}`}>{name}</p>
-            <p>{score}</p>
+          {ranking.map((player, index) => (
+            <li key={`${player.name}${player.score}`}>
+              <img src={defaultImg} alt={`${player.name} Avatar`} />
+              <p data-testid={`player-name-${index}`}>{player.name}</p>
+              <p>{player.score}</p>
           </li>
+          ))}
         </ul>
       </div>
     );
   }
 }
-
-RankingList.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  score: PropTypes.number.isRequired,
-  index: PropTypes.number.isRequired,
-};
-
-// const mapStateToProps = (state) => ({
-//   avatar: state.ranking.avatar,
-//   name: state.ranking.name,
-//   score: state.ranking.score,
-// });
-
-// export default connect(mapStateToProps)(RankingList);
 
 export default RankingList;
