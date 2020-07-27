ff4d49a7d44f22581ad2cc937095ebbee02f3543
gabriellukke <gabriellukke@outlook.com>
2020-07-27 17:00:55 -0300

    CC v2.0.0

--- src/reducers/reducerPlayer.js
+++ src/reducers/reducerPlayer.js
@@ -4,17 +4,19 @@ const initialState = {
   player: {
     gravatarEmail: '',
     name: '',
-  }
+  },
 };
 
 const reducerPlayer = (state = initialState, action) => {
   switch (action.type) {
     case PLAYER_ADD:
       return {
-        ...state, player: {
+        ...state,
+        player: {
           ...state.player,
-          gravatarEmail: action.player.email, name: action.player.name
-        }
+          gravatarEmail: action.player.email,
+          name: action.player.name,
+        },
       };
     default:
       return state;
